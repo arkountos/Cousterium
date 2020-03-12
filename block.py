bffbcfec18db4b551e4e1bf9b6fb5e2cb3415aca
pep <ath.v.peppas@gmail.com>
2020-03-13 01:25:50 +0200

    Constructor and hash function implementation

--- block.py
+++ block.py
@@ -1,24 +1,28 @@
 # Should import blockchain?
 # import blockchain
-
+import datetime
+from haslib import sha256
+import json
 
 
 
 class Block:
-	def __init__(self):
+	def __init__(self, *nonce, previousHash):
 		##set
-
-		#self.previousHash
-		#self.timestamp
-		#self.hash
-		#self.nonce
-		#self.listOfTransactions
+		
+		self.previousHash = previousHash
+		self.timestamp = datetime.datetime.utcnow()
+		self.hash
+		if nonce == None:
+			self.nonce
+		else:
+			self.nonce = 0 
+		self.listOfTransactions = []
 		pass
 	
 	def myHash():
-		#calculate self.hash
-		pass
-
+		block_string = json.dumps(self.__dict__, sort_keys=True)
+    	return sha256(block_string.encode()).hexdigest()
 
 	def add_transaction(transaction, blockchain):
 		#add a transaction to the block
