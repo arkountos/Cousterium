e50ebc8208b291ae8f4a5054ecc909674d19c4ed
konkou <konkou97@hotmail.com>
2020-03-13 17:13:47 +0200

    Added wallet constructor and RSA generator

--- wallet.py
+++ wallet.py
@@ -16,14 +16,17 @@ from uuid import uuid4
 
 class Wallet:
 
-	def __init__(self):
-		##set
-
-		#self.public_key
-		#self.private_key
-		#self_address
-		#self.transactions
-		pass
+	def __init__(self, public_key=None, private_key=None, address, transactions):
+		
+		self.public_key = public_key
+		self.private_key = private_key
+		self.address = address
+		self.transactions = transactions
+		
+	def generate_RSA():
+		new_keypair = RSA.generate(2048)
+		public_key = new_keypair.publickey().exportKey('PEM').decode()
+		private_key = new_keypair.exportKey('PEM').decode()
 
 	def balance():
 		pass
