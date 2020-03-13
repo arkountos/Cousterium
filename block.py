b0c426793fd3ccffc642ad74df109ea2df17bcd6
pep <ath.v.peppas@gmail.com>
2020-03-13 21:56:18 +0200

    hash generator func

--- block.py
+++ block.py
@@ -4,26 +4,26 @@ import datetime
 from hashlib import sha256
 import json
 
-
-
 class Block:
-	def __init__(self, *nonce, previousHash):
+	def __init__(self, id, previousHash, transactions, *nonce):
 		##set
 		
+		self.id = id
 		self.previousHash = previousHash
 		self.timestamp = datetime.datetime.utcnow()
-		self.hash
+		#self.hash 
 		if nonce == None:
-			self.nonce
+			self.nonce = nonce
 		else:
 			self.nonce = 0 
 		self.listOfTransactions = []
-		pass
+		self.listOfTransactions.append(transactions)
 	
-	def myHash():
-		block_string = json.dumps(self.__dict__, sort_keys=True)
-    	return sha256(block_string.encode()).hexdigest()
+	def myHash(self):
+		block_string = str(self.nonce)
+		return (sha256(block_string.encode()).hexdigest())
 
 	def add_transaction(transaction, blockchain):
 		#add a transaction to the block
 		pass
+
