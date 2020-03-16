916eacee9ddac348070c28d9ff62e250a9ed4afc
pep <ath.v.peppas@gmail.com>
2020-03-16 20:45:10 +0200

    minor changes

--- block.py
+++ block.py
@@ -6,25 +6,23 @@ import json
 capacity = 10 
 
 class Block:
-	def __init__(self, id, previousHash, listOfTransactions, *nonce):
+	def __init__(self, id, previousHash, current_hash, listOfTransactions, nonce=0):
 		##set
 		
 		self.id = id
 		self.previousHash = previousHash
 		self.timestamp = datetime.datetime.utcnow()
-		#self.hash 
-		if nonce == None:
-			self.nonce = nonce
-		else:
-			self.nonce = 0 
+		self.current_hash == current_hash   
+		self.nonce = nonce 
 		self.listOfTransactions = []
 
+	def dump(self):
+		return json.dumps(dict(transactions=self.transactions,nonce=self.nonce,timestamp=self.timestamp), sort_keys=True)
+
 	def myHash(self):
-		block_string = str(self.nonce)
-		return (sha256(block_string.encode()).hexdigest())
+		return (sha256.new(self.dump().encode()).hexdigest())
 
 	def add_transaction(self, transaction):
 		if len(self.listOfTransactions) < capacity:
 			self.listOfTransactions.append(transaction)
 		pass
-
