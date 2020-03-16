700cb700b65f2e27093ee998c5d1ad7138776bce
pep <ath.v.peppas@gmail.com>
2020-03-16 21:15:27 +0200

    further implementation of blockchain method

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,6 @@
 import block
-import transaction
-import wallet 
+#import transaction
+#import wallet 
 
 class Blockchain:
     difficulty = 5
@@ -13,7 +13,6 @@ class Blockchain:
         genesis_block.current_hash = genesis_block.myHash()
         self.chain.append(genesis_block)
         
-
     def proof_of_work(self, block):
         block.nonce = 0
         current_hash = block.myHash()
@@ -23,6 +22,24 @@ class Blockchain:
             current_hash = block.myHash()
         
         return current_hash
+
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and block_hash == block.compute_hash())
+
+    def add_block(self, block, proof):
+
+        previousHash = self.last_block().previousHash
+
+        if previousHash != block.previousHash:
+            return False
+
+        if not Blockchain.is_valid_proof(block, proof):
+            return False
+
+        block.current_hash = proof
+        self.chain.append(block)  
+        
+        return True   
 
     def last_block(self):
         return self.chain[-1]
