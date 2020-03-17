1ffda83714a7ab46eebfadb336151527a486e528
arkountos <eternallistener@gmail.com>
2020-03-17 17:01:32 +0200

    Changes to IP script

--- ip.py
+++ ip.py
@@ -7,4 +7,8 @@ for i in interfaces:
     iface = netifaces.ifaddresses(i).get(netifaces.AF_INET)
     if iface != None:
         for my_j in iface:
-            print (my_j['addr'])
+           # print (my_j['addr'])
+           pass
+
+iface = netifaces.ifaddresses('eth1').get(netifaces.AF_INET)
+print(iface[0]["addr"])
