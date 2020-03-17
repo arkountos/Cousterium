8ebe002dbb83fdd5f729e2a7afebc2d989d65330
Fivos Iliadis <eternallistener@gmail.com>
2020-03-17 15:40:30 +0200

    Moved everything inside rest.py, added script to find private IP

--- rest.py
+++ rest.py
@@ -4,36 +4,92 @@ import block
 import node
 import wallet
 import transaction
+import socket
+import json
+import requests
+
+# Setup the bootstrap node
+def setup_bootstrap_node():
+    # Create the very first node
+    # The node constructor also creates the Wallet() we need and binds it to the node (I hope)
+    myNode = node.Node(0, MY_ADDRESS)
+    print(myNode)
+
+    # Create the genesis block with id = 0 and prev_hash = -1
+    genesis_block = block.Block(0, -1, []) # TODO: we shouldn't have to pass emtpy list as listOfTransactions in constructor, see with peppas
+
+    # Need to add the first and only transaction to the genesis block
+    first_transaction = transaction.Transaction(0, MY_ADDRESS, NETWORK_SIZE * 100)
+    genesis_block.add_transaction(first_transaction)
+
+    # Add the first block to the node's blockchain
+    myNode.chain.append(genesis_block)
+
+    # Return the node
+    return (myNode)
+
+# Setup a regular participant node
+def setup_regular_node():
+    # Create node and associate with VM via address
+    myNode = node.Node(0, MY_ADDRESS)
+    print(myNode)
+
+    # Our node sends it's publik key (= wallet address = MY_ADDRESS ? )
+    # and receives a unique id (0..NETWORK_SIZE) 
+    print('http://' + SERVER_ADDRESS + ':5000/add_to_ring')
+    r = requests.post('http://' + SERVER_ADDRESS + ':5000/add_to_ring', data = {'public_key':MY_ADDRESS})
+    print(r)
+    print(r.text)
+
+# A function to get the VM's private IP (e.g. 192.168.0.4)
+def get_my_ip():
+    """
+    Find my IP address
+    :return:
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("1.1.1.1", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
 
 app = Flask(__name__)
 
-# # If you hit that you are setting up yourself to be the bootstrap node
-# @app.route('/setup/bootstrap', methods=['GET'])
-# def setup_bootstrap_node():
-#     # This node private IP Address
-#     MY_ADDRESS = '192.168.0.2'
-
-#     # Number of nodes in network
-#     NETWORK_SIZE = 5
-
-#     # Create the very first node
-#     # The node constructor also creates the Wallet() we need and binds it to the node (I hope)
-#     myNode = node.Node(0, MY_ADDRESS)
-#     print(myNode)
-
-#     # Create the genesis block with id = 0 and prev_hash = -1
-#     genesis_block = block.Block(0, -1)
-
-#     # Need to add the first and only transaction to the genesis block
-#     first_transaction = transaction.Transaction(0, MY_ADDRESS, NETWORK_SIZE * 100)
-#     genesis_block.add_transaction(first_transaction)
-
-#     # Add the first block to the node's blockchain
-#     myNode.chain.append(genesis_block)
+#### IF BOOTSTRAP NODE ####
+if (get_my_ip() == '192.168.0.2'):
+    MY_ADDRESS = '192.168.0.2'
+    # Number of nodes in network
+    NETWORK_SIZE = 5
+    #IDs
+    NODE_IDS = [4, 3, 2, 1]
+    # IP addresses of all nodes in ring
+    ADDRESS_BOOK = [MY_ADDRESS]
+    MY_NODE = setup_bootstrap_node()
+#### IF REGULAR NODE ####
+else:
+    MY_ADDRESS = get_my_ip()
+    # Bootstrap node address (we suppose it is known to everyone)
+    SERVER_ADDRESS = '192.168.0.2'
+    MY_NODE = setup_regular_node()
 
 
+# Add the calling node to the ring
+# 1) Add the node to the ring
+# 2) Send back an id
+@app.route('/add_to_ring', methods=['POST'])
+def add_to_ring():
+    next_id = NODE_IDS.pop()
+    print("Got it")
+    print(request.form.to_dict())
+    # 1)
+    MY_NODE.chain.append()
+    # 2)
+    return (next_id)
 
-
+@app.route('/test')
+def test():
+    print('Address: ' + json.dumps(MY_ADDRESS))
+    return('Address: ' + json.dumps(MY_ADDRESS))
 
 # get all transactions in the blockchain
 
@@ -43,7 +99,6 @@ def get_transactions():
 
     response = {'transactions': transactions}
     return jsonify(response), 200
-
 
 
 # run it once fore every node
