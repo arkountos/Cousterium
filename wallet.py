f6e44d233c54ae01a9809d79703571f617562215
arkountos <eternallistener@gmail.com>
2020-03-19 20:40:11 +0200

    wallet actually creates keys and assings them to self.fields on creation

--- wallet.py
+++ wallet.py
@@ -18,8 +18,9 @@ class Wallet:
 
 	def __init__(self, address, transactions, public_key=None, private_key=None):
 		
-		self.public_key = public_key
-		self.private_key = private_key
+		keys = self.generate_RSA()
+		self.public_key = keys[0]
+		self.private_key = keys[1]
 		self.address = address
 		self.transactions = transactions
 		
@@ -27,6 +28,8 @@ class Wallet:
 		new_keypair = RSA.generate(2048)
 		public_key = new_keypair.publickey().exportKey('PEM').decode()
 		private_key = new_keypair.exportKey('PEM').decode()
+		# print([public_key, private_key])
+		return [public_key, private_key]
 
 	def balance(self, UTXOS):
     	# just a prototype. UTXOS is the list of all uspent transactions
